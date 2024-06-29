f3d754c78d4e0cfd3fed184c44c5f866b1cb08e2
fulaytar <fulaytar2405@gmail.com>
2024-06-29 20:21:24 +0300

    крок 2 ок

--- tetris.js
+++ tetris.js
@@ -1,23 +1,69 @@
-const tetrisRows = 10;
-const tetrisColums = 20;
+const tetrisRows = 20;
+const tetrisColums = 10;
 let playfield;
 
-const tetroMino_Names = ['0'];
+const tetroMino_Names = ['O', 'L', 'J', 'S', 'Z', 'I', 'T'];
 
 const tetroMinoes = {
-  0: [[1]],
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  L: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  J: [
+    [0, 1, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  I: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  T: [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
 };
 
 let tetromino = {
   name: '',
-  matrix: '',
+  matrix: [],
   column: 0,
   row: 0,
 };
 
+//COMMON
+function convertPositionToIndex(row, colum) {
+  return row * tetrisColums + colum;
+}
+
+function randomFigure(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+//Generete
 function genereteTetromino() {
-  const nameTetro = tetroMino_Names[0];
-  const matrix = tetroMinoes[0];
+  const nameTetro = randomFigure(tetroMino_Names);
+  const matrix = tetroMinoes[nameTetro];
+
   const columnTetro = 4;
   const rowTetro = 5;
 
@@ -29,38 +75,97 @@ function genereteTetromino() {
   };
 }
 
-function drawPlayField() {
-  playfield[4][8] = 'activeBlock';
-
-  for (let row = 0; row < tetrisRows; row++) {
-    for (let colum = 0; colum < tetrisColums; colum++) {
-      if (!playfield[row][colum]) continue;
-      const nameFidure = 'activeBlock';
-      const blockIndex = convertPositionToIndex(row, colum);
-      allBlockTetris[blockIndex].classList.add(nameFidure);
-    }
-  }
-}
-function convertPositionToIndex(row, colum) {
-  return row * tetrisColums + colum;
-}
-
 function gerenetePlayField() {
-  for (let i = 0; i < tetrisRows * tetrisColums; i++) {
+  for (let i = 0; i < tetrisColums * tetrisRows; i++) {
     const tetrisBlock = document.createElement('li');
     tetrisBlock.className = 'tetris_block';
+    /* tetrisBlock.textContent = i; */
     document.querySelector('.tetris').append(tetrisBlock);
   }
+
   playfield = new Array(tetrisRows)
     .fill()
     .map(() => new Array(tetrisColums).fill(0));
 }
+
+//KEYBOARD
+document.addEventListener('keydown', onKeyDown);
+
+function onKeyDown(event) {
+  if (event.key === 'ArrowLeft') {
+    tetromino.column -= 1;
+    if (isOutSideGameboard(tetromino.row, tetromino.column)) {
+      tetromino.column += 1;
+    }
+  }
+  if (event.key === 'ArrowRight') {
+    tetromino.column += 1;
+    if (isOutSideGameboard(tetromino.row, tetromino.column)) {
+      tetromino.column -= 1;
+    }
+  }
+  if (event.key === 'ArrowDown') {
+    tetromino.row += 1;
+    if (isOutSideGameboard(tetromino.row, tetromino.column)) {
+      tetromino.row -= 1;
+    }
+  }
+  draw();
+}
+
+function draw() {
+  drawPlayField();
+  allBlockTetris.forEach(el => el.classList.remove(`${tetromino.name}`));
+  drawTetramino();
+}
+
+// COLLISIONS
+
+function isOutSideGameboard(row, column) {
+  return (
+    column < 0 ||
+    column > tetrisColums - tetromino.matrix.length ||
+    row > tetrisRows - tetromino.matrix.length - 1
+  );
+}
+
+//DRAW
+function drawTetramino() {
+  const name = tetromino.name;
+  const tetrominoMatrixSize = tetromino.matrix.length;
+
+  for (let row = 0; row < tetrominoMatrixSize; row++) {
+    for (let column = 0; column < tetrominoMatrixSize; column++) {
+      if (!tetromino.matrix[row][column]) {
+        continue;
+      }
+      const index = convertPositionToIndex(
+        tetromino.row + row,
+        tetromino.column + column
+      );
+
+      allBlockTetris[index].classList.add(name);
+    }
+  }
+}
+
+function drawPlayField() {
+  for (let row = 0; row < tetrisRows; row++) {
+    for (let column = 0; column < tetrisColums; column++) {
+      if (!playfield[row][column]) continue;
+      const nameFidure = tetromino.name;
+      const blockIndex = convertPositionToIndex(row, column);
+
+      allBlockTetris[blockIndex].classList.add(nameFidure);
+    }
+  }
+}
+
 gerenetePlayField();
 
 let allBlockTetris = document.querySelectorAll('.tetris_block');
 
 genereteTetromino();
-
-drawPlayField();
+draw();
 
 /* https://www.youtube.com/live/hGEPkXBKoBM  1^57 */
